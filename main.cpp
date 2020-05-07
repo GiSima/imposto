fa8b343f5d9c5a26b36f570247151994d3c67ecf
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2020-05-07 09:13:01 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double salario, servico, capital, medico, educa;
+
+    cout << "Renda anual com salario: "; cin >> salario;
+    cout << "Renda anual com prestacao de servico: "; cin >> servico;
+    cout << "Renda anual com ganho de capital: "; cin >> capital;
+    cout << "Gastos medicos: "; cin >> medico;
+    cout << "Gastos educacionais: "; cin >> educa;
+
     return 0;
 }
